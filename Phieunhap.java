ef620cbd3d819484f071713f721a44d6c6c911a7
viet11 <73016156+viet11@users.noreply.github.com>
2021-05-05 18:24:17 +0700

    Add files via upload

--- Phieunhap.java
+++ Phieunhap.java
@@ -27,14 +27,27 @@ public void hienThiThongTinKhachhang() {
 
 }
 }
-public class ChiTietPhieuNhap extends PhieuNhap{
-    private String maSP,tenSp,soLuong,HSD;
+public class Chitietphieunhap extends pn {
+    int MaPhieuNhap;
+    String TenPhieuNhap;
+    int SoLuong;
+    String HanSuDung;
+    
+    void nhap() {
+    	Scanner scanner = new Scanner(System.in);
+         System.out.print("Nhập mã phiếu: ");
+    MaPhieuNhap = scanner.nextInt();
+    System.out.print("Nhập tên phiếu nhập: ");
+    TenPhieuNhap = scanner.nextLine();
+    System.out.print("Nhập số lượng: ");
+    SoLuong = scanner.nextInt();
+    System.out.print("Nhập hạn sử dụng: ");
+    HanSuDung = scanner.nextLine();
+    }
+    void in(){
+         System.out.println("Mã phiếu:"+MaPhieuNhap +"\n" +"Tên Phiếu Nhâp:"+ TenPhieuNhap +"\n"+"Số lượng:"+ SoLuong +"hạn sử dụng: "+HanSuDung);
+    }
     
 }
 }
-
-
-
-
-    
 
